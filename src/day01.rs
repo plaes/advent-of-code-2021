81620e8b44553ba71bd07601abe557d4a41c565b
Priit Laes <plaes@plaes.org>
2021-12-03 23:54:10 +0200

    Improve day01 with windows :)

--- src/day01.rs
+++ src/day01.rs
@@ -12,37 +12,32 @@ const _DATA: &str = "199
 263";
 
 fn main() -> Result<(), Error> {
-    let data = include_str!("../data/day01.txt")
-    //let data = DATA
+    let data: Vec<_> = include_str!("../data/day01.txt")
+        //let data = DATA
         .lines()
         .filter_map(|x| x.parse().ok())
-        .collect::<Vec<u16>>();
+        .collect();
 
     // First part
     // Count the number of times a depth measurement increases from the
     // previous measurement.
-    let inc_1 = data.iter().scan(u16::MAX, |prev, &x| {
-        let inc = *prev < x;
-        *prev = x;
-        Some(inc)
-    }).filter(|x| *x == true).collect::<Vec<bool>>().len();
-
-    println!("Number of increases (1): {:?}", inc_1);
+    println!(
+        "Number of increases (1): {}",
+        data.windows(2).filter(|w| w[0] < w[1]).count()
+    );
 
     // Second part
     // Instead, consider sums of a three-measurement sliding window.
     // Your goal now is to count the number of times the sum of measurements in
-    // this sliding window increases from the previous sum. 
+    // this sliding window increases from the previous sum.
 
-    let window_sums = data.windows(3).map(|x| x.to_vec().iter().sum::<u16>()).collect::<Vec<u16>>();
+    // Build sums for sliding window with size 3
+    let sums: Vec<usize> = data.windows(3).map(|w| w.iter().sum()).collect();
 
-    let inc_2 = window_sums.iter().scan(u16::MAX, |prev, &x| {
-        let inc = *prev < x;
-        *prev = x;
-        Some(inc)
-    }).filter(|x| *x == true).collect::<Vec<bool>>().len();
-
-    println!("Number of increases (2): {:?}", inc_2);
+    println!(
+        "Number of increases (2): {}",
+        sums.windows(2).filter(|w| w[0] < w[1]).count()
+    );
 
     Ok(())
 }
