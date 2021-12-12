52d5d92ab68b18c97ec733e5ad88d48035441588
Priit Laes <plaes@plaes.org>
2021-12-12 23:05:48 +0200

    Add day10 part 1

---
+++ src/day10.rs
@@ -0,0 +1,52 @@
+use std::collections::HashMap;
+
+const _DATA: &str = "[({(<(())[]>[[{[]{<()<>>
+[(()[<>])]({[<{<<[]>>(
+{([(<{}[<>[]}>{[]{[(<()>
+(((({<>}<{<{<>}{[]{[]{}
+[[<[([]))<([[{}[[()]]]
+[{[{({}]{}}([{[{{{}}([]
+{<[[]]>}<{[{[{[]{()[[[]
+[<(<(<(<{}))><([]([]()
+<{([([[(<>()){}]>(<<{{
+<{([{{}}[<[[[<>{}]]]>[]]";
+
+fn main() {
+    let data = include_str!("../data/day10.txt");
+    //let data = _DATA;
+
+    let elem: HashMap<char, char> = HashMap::from([
+        (')', '('),
+        (']', '['),
+        ('}', '{'),
+        ('>', '<'),
+    ]);
+
+    let mut sum = 0;
+
+    let mut stack: Vec<char> = vec![];
+    for line in data.lines() {
+        for c in line.chars() {
+            if matches!(c, '(' | '[' | '{' | '<') {
+                stack.push(c);
+            } else if matches!(&c, ')' | ']' | '}' | '>') {
+                let f = elem.get(&c).unwrap().to_owned();
+                if stack.pop() != Some(f) {
+                    // println!("Failed: {:?}", c);
+                    sum += match c {
+                        ')' => 3,
+                        ']' => 57,
+                        '}' => 1197,
+                        '>' => 25137,
+                        _ => unreachable!(),
+                    };
+                    break;
+                }
+            } else {
+                unreachable!();
+            }
+        }
+    }
+
+    println!("Total syntax error score (part1): {}", sum);
+}
