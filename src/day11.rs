9f2553631a83cf0384940385cc9a93337fb66fed
Priit Laes <plaes@plaes.org>
2021-12-23 22:31:32 +0200

    Day11 part1

--- src/day11.rs
+++ src/day11.rs
@@ -1,8 +1,21 @@
+/*
 const _DATA: &str = "11111
 19991
 19191
 19991
 11111";
+*/
+
+const _DATA: &str = "5483143223
+2745854711
+5264556173
+6141336146
+6357385478
+4167524645
+2176841721
+6882881134
+4846848554
+5283751526";
 
 #[rustfmt::skip]
 const adjacent: [(isize, isize); 8] = [
@@ -12,19 +25,17 @@ const adjacent: [(isize, isize); 8] = [
 ];
 
 fn flash(map: &mut Vec<Vec<u8>>, x: usize, y: usize) -> usize {
-    // Set current location to 0
-    println!("{},{}", x, y);
+    // Set value of current item to 0 (and count it with fold initializer)
     map[x][y] = 0;
     // Iterate over adjacent coords...
     adjacent
         .iter()
         .map(|(xx, yy)| ((x as isize + xx) as usize, (y as isize + yy) as usize))
-        .fold(1, |sum, (x, y)| {
-            println!("({},{})", x, y);
-            match map.get_mut(y).and_then(|line| line.get_mut(x)) {
+        .fold(1, |sum, (xxx, yyy)| {
+            match map.get_mut(xxx).and_then(|line| line.get_mut(yyy)) {
                 Some(item) if *item > 0 => {
                     *item += 1;
-                    sum + (*item > 9).then(|| flash(map, x, y)).unwrap_or(0)
+                    sum + (*item > 9).then(|| flash(map, xxx, yyy)).unwrap_or(0)
                 }
                 _ => sum,
             }
@@ -32,8 +43,8 @@ fn flash(map: &mut Vec<Vec<u8>>, x: usize, y: usize) -> usize {
 }
 
 fn main() {
-    //let data = include_str!("../data/day09.txt");
-    let data = _DATA;
+    let data = include_str!("../data/day11.txt");
+    //let data = _DATA;
 
     // Part 1
     // Parse map
@@ -42,27 +53,25 @@ fn main() {
         .map(|l| {
             l.chars()
                 .map(|n| n.to_string().parse::<u8>().unwrap())
-                .collect()
+                .collect::<Vec<_>>()
         })
-        .collect::<Vec<Vec<_>>>();
+        .collect::<Vec<_>>();
 
     // Part 1
     // XXX: Simplification assuming that we always have n x n map.. :(
     let N = map.len();
-
-    (0..=2).fold(0, |sum, _| {
-        println!("{:?}", map);
-
-        // Increase all the items...
+    let total = (0..100).fold(0, |sum, _| {
+        // First, run through all the coords and increase them by one...
         map.iter_mut()
             .for_each(|line| line.iter_mut().for_each(|item| *item += 1));
-        // Run over the maze and collect flashes recursively...
+        // ...iterate over the maze and recursively handle flashes...
         sum + (0..N)
-            .flat_map(|y| (0..N).map(move |x| (x, y)))
+            .flat_map(|x| (0..N).map(move |y| (x, y)))
             .fold(0, |sum, (x, y)| {
                 sum + (map[x][y] > 9).then(|| flash(&mut map, x, y)).unwrap_or(0)
             })
     });
+    println!("Total flashes after 100 steps (part1): {}", total);
 
     // Part 2
     // TODO
